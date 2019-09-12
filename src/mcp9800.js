35a529c47a9ea635e7d06d74331c13b0ce9e63b1
Nico De Witte <nico.dewitte@outlook.com>
2019-09-12 20:35:34 +0200

    Read temperature from sensor

--- src/mcp9800.js
+++ src/mcp9800.js
@@ -1,7 +1,21 @@
+const registers = {
+  TEMPERATURE: 0,
+  CONFIG: 1,
+  HYSTERESIS: 2,
+  LIMIT_SET: 3
+};
+
 class Mcp9800 {
 
-  constructor(i2c) {
+  constructor(i2c, devAddress=0x48) {
     this.i2c = i2c;
+    this.devAddress = devAddress;
+  }
+
+  temperature() {
+    let buffer = Buffer.alloc(2, 0x00);
+    this.i2c.readI2cBlockSync(this.devAddress, registers.TEMPERATURE, buffer.length, buffer);
+    return buffer[0] + (buffer[1]/256.0);
   }
 
 }
